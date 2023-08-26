e04ca6a75ec63d0ae171f8a10ce4d17469147ef8
eunjian <46674129+eunjian@users.noreply.github.com>
2023-08-26 23:38:40 +0200

    Update answer.py

--- day4/answer.py
+++ day4/answer.py
@@ -1,26 +1,26 @@
-n = int(input()) #재료 갯수 입력
+n = int(input())  # Input the number of ingredients
 
-taste_list = list(map(int, input().split())) #각 재료별 맛의 정도 입력
-s = 0 #재료의 맛의 정도의 합
+taste_list = list(map(int, input().split()))  # Input the taste levels for each ingredient
+s = 0  # Sum of taste levels of ingredients
 
 for i in range(n):
-	#최대값 이전까지는, 재료의 맛의 정도가 증가하거나 다음 요소와 같아야 함
-	if i < taste_list.index(max(taste_list)): 
-		if taste_list[i] > taste_list[i+1]: #재료의 맛의 정도가 감소했을 경우
-			s = 0 #완벽하지 않은 햄버거이므로 햄버거의 맛은 0이 됨
-			break #s가 0이 될 경우 더는 반복문을 진행할 필요 없음
-		else:
-			s += taste_list[i]
-	#최댓값의 경우, 재료 맛의 합 리스트에 추가
-	elif i == taste_list.index(max(taste_list)):
-		s += taste_list[i]
-	#최대값 이후에는 재료의 맛의 정도가 감소하거나 이전 요소와 같아야함
-	else:
-		if taste_list[i] > taste_list[i-1]: #맛의 정도가 증가했을 경우
-			s = 0
-			break
-		else:
-			s += taste_list[i]
+    # Up to the point before the maximum value, ingredient taste should increase or stay the same as the next element
+    if i < taste_list.index(max(taste_list)):
+        if taste_list[i] > taste_list[i+1]:  # If ingredient taste decreases
+            s = 0  # The burger taste is imperfect, so the burger's taste becomes 0
+            break  # If s becomes 0, there's no need to continue the loop
+        else:
+            s += taste_list[i]
+    # For the maximum value, add the ingredient taste to the sum
+    elif i == taste_list.index(max(taste_list)):
+        s += taste_list[i]
+    # After the maximum value, ingredient taste should decrease or stay the same as the previous element
+    else:
+        if taste_list[i] > taste_list[i-1]:  # If taste increases
+            s = 0
+            break
+        else:
+            s += taste_list[i]
 
-#재료의 맛의 정도의 합을 출력
+# Print the sum of ingredient taste levels
 print(s)

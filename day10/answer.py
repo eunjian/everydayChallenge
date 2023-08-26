5337b23f96a2febb2035485f42af90703d08b902
eunjian <46674129+eunjian@users.noreply.github.com>
2023-08-27 00:06:00 +0200

    Update answer.py

--- day10/answer.py
+++ day10/answer.py
@@ -1,64 +1,73 @@
 import numpy as np
-# 게임판의 크기 입력
+
+# Input the size of the game board
 n = int(input())
-# 구름이와 플레이어가 말을 올려둔 칸의 좌표
+
+# Input the coordinates where the Goorm and player placed their tokens
 r_g, c_g = map(int, input().split())
 r_p, c_p = map(int, input().split())
-# 보드의 각칸에 적혀있는 지시 사항
+
+# Input the instructions written on each cell of the board
 arr = [list(input().split()) for _ in range(n)]
-org = [[r_g,c_g],[r_p,c_p]]
+
+# Create a matrix to track the movements of Goorm and the player
+org = [[r_g, c_g], [r_p, c_p]]
 result = []
 
+# Iterate through the movements of both Goorm and the player
 for xy in org:
-	repeat = True
-	# 구름이와 플레이어의 이동 확인을 위한 게임판 생성
-	b = [[0] * n for _ in range(n)]
-	x = xy[0]-1
-	y = xy[1]-1
-	b[x][y] = 1
-	while repeat:
-		c = int(arr[x][y][:-1]) #이동할 칸의 수
-		if 'U' in arr[x][y]: #위쪽이동
-			for i in range(c):
-				x -= 1
-				if x < 0:
-					x = n-1
-				if b[x][y] != 0:
-					repeat = False
-					break
-				b[x][y] = 1
-		elif 'D' in arr[x][y]: #아래쪽 이동
-			for i in range(c):
-				x += 1
-				if x >= n:
-					x = 0
-				if b[x][y] != 0:
-					repeat = False
-					break
-				b[x][y] = 1
-		elif 'L' in arr[x][y]: #왼쪽 이동
-			for i in range(c):
-				y -= 1
-				if y < 0:
-					y = n-1
-				if b[x][y] != 0:
-					repeat = False
-					break
-				b[x][y] = 1
-		else: # 오른쪽 이동
-			for i in range(c):
-				y += 1
-				if y >= n:
-					y = 0
-				if b[x][y] != 0:
-					repeat = False
-					break
-				b[x][y] = 1
-	# result.append(b)
-	result.append(np.array(b).flatten().tolist().count(1))
-		
-# 구름이와 플레이어의 이동 횟수 비교
+    repeat = True
+
+    # Create a game board to track the movements
+    b = [[0] * n for _ in range(n)]
+    x = xy[0] - 1
+    y = xy[1] - 1
+    b[x][y] = 1
+
+    while repeat:
+        c = int(arr[x][y][:-1])  # Number of cells to move
+        if 'U' in arr[x][y]:  # Move upwards
+            for i in range(c):
+                x -= 1
+                if x < 0:
+                    x = n - 1
+                if b[x][y] != 0:
+                    repeat = False
+                    break
+                b[x][y] = 1
+        elif 'D' in arr[x][y]:  # Move downwards
+            for i in range(c):
+                x += 1
+                if x >= n:
+                    x = 0
+                if b[x][y] != 0:
+                    repeat = False
+                    break
+                b[x][y] = 1
+        elif 'L' in arr[x][y]:  # Move left
+            for i in range(c):
+                y -= 1
+                if y < 0:
+                    y = n - 1
+                if b[x][y] != 0:
+                    repeat = False
+                    break
+                b[x][y] = 1
+        else:  # Move right
+            for i in range(c):
+                y += 1
+                if y >= n:
+                    y = 0
+                if b[x][y] != 0:
+                    repeat = False
+                    break
+                b[x][y] = 1
+
+    # Count the number of cells visited by each player and store it in the result list
+    result.append(np.array(b).flatten().tolist().count(1))
+
+# Compare the number of cells visited by Goorm and player, then print the result
 if result[0] > result[1]:
-	print('goorm', result[0])
+    print('goorm', result[0])
 else:
-	print('player', result[1])
+    print('player', result[1])

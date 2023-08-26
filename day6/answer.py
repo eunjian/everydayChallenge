25fe2f10dcd6576ddcc39c52fe917baa5a335e01
eunjian <46674129+eunjian@users.noreply.github.com>
2023-08-26 23:43:08 +0200

    Update answer.py

--- day6/answer.py
+++ day6/answer.py
@@ -1,28 +1,30 @@
 from itertools import combinations
-n = int(input()) #문자열의 길이
-s = input() #문자열
 
-p = list() #부분문자열
-d = list() #3개로 나뉜 문자열
-for i in range(n-2):
-	for j in range(i+1,n-1):
-		#3개로 나눈 문자열 저장
-		d.append([s[:i+1], s[i+1:j+1], s[j+1:]])
-		#부분 문자열 저장
-		p.append(s[:i+1])
-		p.append(s[i+1:j+1])
-		p.append(s[j+1:])
+n = int(input())  # Input the length of the string
+s = input()  # Input the string
 
-#부분문자열 중 중복 제거 후 사전 순서 배열
+p = list()  # Substrings
+d = list()  # Split strings into 3 parts
+
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        # Save the string split into 3 parts
+        d.append([s[:i + 1], s[i + 1:j + 1], s[j + 1:]])
+        # Save the substrings
+        p.append(s[:i + 1])
+        p.append(s[i + 1:j + 1])
+        p.append(s[j + 1:])
+
+# Remove duplicates from substrings and sort them in lexicographical order
 p = sorted(set(p))
 
-#최대 점수 계산
+# Calculate the maximum score
 score = list()
 for i in range(len(d)):
-	s = 0 #점수
-	for item in d[i]:
-		s += p.index(item)+1
-		score.append(s)
+    s = 0  # Score
+    for item in d[i]:
+        s += p.index(item) + 1
+    score.append(s)
 
-#최대 점수 출력
+# Print the maximum score
 print(max(score))

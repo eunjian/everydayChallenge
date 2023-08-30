3b396c95ef956cffecaabca72577361d45d966b4
eunjian <46674129+eunjian@users.noreply.github.com>
2023-08-30 23:16:07 +0200

    Update solution.py

--- day11/solution.py
+++ day11/solution.py
@@ -11,6 +11,3 @@ for i in range(N + 1):
 		dp[i] = min(dp[i], dp[i - B] + 1)
 
 print(dp[N] if dp[N] != float('inf') else -1)
-
-# 내 코드랑 다른점/ 배울점
-# 나는 for문을 2번 사용해서 반복했는데 정해 코드에선 if문을 두번 써서 아이템 A와 B를 각각 비교함

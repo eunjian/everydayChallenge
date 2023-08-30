a95b8eb5f3c48b69b80c7deacb0d4d2e39f31e83
eunjian <46674129+eunjian@users.noreply.github.com>
2023-08-30 23:20:50 +0200

    Update answer.py

--- day13/answer.py
+++ day13/answer.py
@@ -1,45 +1,45 @@
 from collections import deque
 
-# bfs를 이용해 상하좌우 탐색하여 동일 유형 건물 개수 구하기
+# Using BFS to explore the adjacent cells (up, down, left, right) and count buildings of the same type
 def bfs(x, y, building_type, village, n):
 	q = deque([(x, y)])
-	village[x][y] = 0 # 방문한 칸 0 표시
-	count = 1 # 건물 개수
-	directions = [(1, 0), (-1, 0), (0, 1), (0, -1)] # 방향 벡터
+	village[x][y] = 0  # Mark the visited cell as 0
+	count = 1  # Count of buildings
+	directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # Direction vectors
 	while q:
 		x, y = q.popleft()
 		for dx, dy in directions:
 			nx, ny = x + dx, y + dy
-			# 좌표의 범위, 방문칸, 동일한 유형의 건물인지 확인
+			# Check if coordinates are in range, unvisited, and have the same building type
 			if 0 <= nx < n and 0 <= ny < n and village[nx][ny] != 0 and village[nx][ny] == building_type:
 				q.append((nx, ny))
 				village[nx][ny] = 0
 				count += 1
 	return count
 
-# 가장 많은 단지가 있는 건물 유형 찾기
+# Finding the building type with the most neighborhoods
 def find_most_building_type(village, n, k):
-	building_counts = {}  # 각 건물 유형의 단지 수를 저장
+	building_counts = {}  # Store the count of neighborhoods for each building type
 	for building_type in range(1, 31):
-		count = 0 # 단지 수
+		count = 0  # Count of neighborhoods
 		for i in range(n):
 			for j in range(n):
 				if village[i][j] == building_type:
 					building_count = bfs(i, j, building_type, village, n)
-					# k개 이상의 동일 유형의 건물이 있을 때 단지로 카운드
-					if building_count >= k: 
+					# Count it as a neighborhood if there are at least k buildings of the same type
+					if building_count >= k:
 						count += 1
-		# 이미 해당 단지 수에 대한 건물 유형 있으면 더 높은 숫자의 건물 선택
+		# If there's a building type already associated with the same count, choose the one with higher number
 		if building_counts.get(count) is None or building_type > building_counts[count]:
 			building_counts[count] = building_type
 	max_building_type = building_counts[max(building_counts.keys())]
 	return max_building_type
 
-# 마을의 크기와 단지의 기준 입력
+# Input the size of the village and the criterion for a neighborhood
 n, k = map(int, input().split())
-# 마을 상태 입력
+# Input the state of the village
 village = [list(map(int, input().split())) for _ in range(n)]
 
-# 가장 많은 단지 수를 가진 건물 유형 찾기
+# Finding the building type with the most neighborhoods
 result = find_most_building_type(village, n, k)
 print(result)

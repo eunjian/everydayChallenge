2e2315802ebacc3017604a526d0e368e2767d52e
eunjian <46674129+eunjian@users.noreply.github.com>
2023-08-30 23:18:12 +0200

    Update answer.py

--- day11/answer.py
+++ day11/answer.py
@@ -1,24 +1,24 @@
-# 플레이어의 통증 수치 입력
+# Receive the player's pain level as input.
 n = int(input())
 
-# 아이템 A와 B가 줄일 수 있는 통증 수치 입력
+# Receive the pain reduction values for items A and B as input.
 a, b = map(int, input().split())
 
-# 아이템의 통증 수치 리스트에 저장
+# Store the pain reduction values of items A and B in a list.
 items = [a, b]
 
-# 최소 아이템 개수 저장하는 리스트 초기화
+# Initialize a list to store the minimum number of items needed.
 d = [1000001] * (n + 1)
 d[0] = 0
 
-# A와 B 각각 반복
+# Iterate over items A and B.
 for i in range(2):
     for j in range(items[i], n + 1):
-        # 현재 통증 수치 j에 대해 최소 아이템 개수 계산
+        # Calculate the minimum number of items required to achieve the current pain level j.
         d[j] = min(d[j], d[j - items[i]] + 1)
 
-# 플레이어의 통증 수치를 0으로 만드는 데 필요한 최소 아이템 개수 출력
-# 만약 통증 수치를 0으로 만들 수 없는 경우, -1 출력
+# Print the minimum number of items needed to reduce the player's pain level to 0.
+# If it's not possible to reduce the pain level to 0, print -1.
 if d[n] == 1000001:
     print(-1)
 else:

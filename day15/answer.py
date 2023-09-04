8529e4f007e1fa8b596124fd326c9a70d20f892c
eunjian <46674129+eunjian@users.noreply.github.com>
2023-09-04 12:32:03 +0200

    Update answer.py

--- day15/answer.py
+++ day15/answer.py
@@ -1,20 +1,26 @@
-# 과일의 개수와 플레이어가 가진 돈 입력
+# Input the number of fruits and the player's budget
 n, k = map(int, input().split())
-# 각 과일의 가격과 포만감 입력
-l = []
+
+# Input the price and satiety of each fruit
+fruits = []
 for _ in range(n):
-	p, c = map(int, input().split())
-	l.append([p,c])
+    price, satiety = map(int, input().split())
+    fruits.append([price, satiety])
 
-# 가격대비 포만감 높은 것부터 정렬, 가격대비 포만감 같을 땐 1개당 가격 높은 순
-l.sort(key=lambda x: (x[1]/x[0], x[0]), reverse=True)
+# Sort fruits by satiety-to-price ratio in descending orde
+fruits.sort(key=lambda x: x[1] / x[0], reverse=True)
 
-f = 0 # 포만감의 합
-for p,c in l:
-	if k >= p:
-		k -= p
-		f += c
-	elif k > 0: # 돈은 남았지만 과일 1개를 전부 살 수는 없을 때 조각으로 구매
-		f += (k/p)*c
-		break
-print(int(f))
+total_satiety = 0  # Total satiety of purchased fruits
+
+for price, satiety in fruits:
+    if k >= price:
+        # If the budget can cover the full price, buy the entire fruit
+        k -= price
+        total_satiety += satiety
+    elif k > 0:
+        # If there's budget left but not enough for a full fruit, buy it in pieces
+        total_satiety += (k / price) * satiety
+        break
+
+# Print the total maximum satiety obtained
+print(int(total_satiety))

7778a9b063664432afdeff7586d69366130f83a1
eunjian <46674129+eunjian@users.noreply.github.com>
2023-09-04 12:29:18 +0200

    Update answer.py

--- day16/answer.py
+++ day16/answer.py
@@ -1,5 +1,5 @@
 from collections import deque
-# BFS 탐색
+# BFS implementation
 def bfs(graph, start, visited):
 	q = deque([start])
 	visited[start] = True
@@ -10,22 +10,22 @@ def bfs(graph, start, visited):
 				q.append(i)
 				visited[i] = True
 				
-# 섬의 개수와 다리의 개수 입력
+# Input of the numbers of islands and bridges
 n, m = map(int, input().split())
-# 다리 정보 입력하여 그래프 생성
+# Create a graph with the input of bridges
 graph = [[] for _ in range(n+1)]
 for i in range(m):
 	n1, n2 = map(int, input().split())
 	graph[n1].append(n2)
-# 방문 노드 체크를 위한 리스트
+# A list for checking visited islands
 visited = [False] * (n+1)
 
-# 연합 개수 세기
+# Count the alliances
 count = 0
 for i in range(1, n+1):
 	if not visited[i]:
 		bfs(graph, i, visited)
 		count += 1
 		
-# 연합 개수 출력
+# Print the number of alliances
 print(count)
